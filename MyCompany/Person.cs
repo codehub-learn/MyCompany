455db8fc1804cbf7718a165fd82d609769370918
dimitris <iracleous@gmail.com>
2021-05-05 19:28:15 +0300

    Add project files.

---
+++ MyCompany/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany
+{
+    public class Person
+    {
+        //properties
+        public string Name { get; set; }
+        public string Email { get; set; }
+ 
+
+    }
+}
---
+++ MyCompany/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCompany
+{
+     // Human resources department
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> persons = new ();
+            persons.Add( new Person
+                {
+                    Name = "Andrew",
+                    Email = "andreas@gmail.com"
+                });
+
+            persons.Add(  new Person
+                {
+                    Name = "Athanassios",
+                    Email = "anthanassios@gmail.com"
+                });
+
+            persons.Add(new Person
+            {
+                Name = "Dimitrios",
+                Email = "dimitrios@gmail.com"
+            });
+
+            foreach(Person person in persons)
+            {
+                Console.WriteLine($"{person.Name}, {person.Email}");
+            }
+
+
+
+        }
+
+        static void DoWork() {   
+            decimal price = 30;
+            int packets = 2;
+            decimal totalPrice = price * packets;
+
+            Console.WriteLine($"Hello. The unit price is {price}, " +
+                $"the number of packes is {packets}" +
+                $" the total price is {totalPrice}");
+        }
+
+    }
+}
